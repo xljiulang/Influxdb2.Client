23f3e626a86174c4a7436dae39fffae13579228b
xljiulang <366193849@qq.com>
2021-03-29 22:10:09 +0800

    HourSelection

---
+++ Influxdb2.Client/FluxExtensions/FluxExtensions.HourSelection.cs
@@ -0,0 +1,18 @@
+﻿namespace Influxdb2.Client
+{
+    public static partial class FluxExtensions
+	{
+		/// <summary>
+		/// 选择时间小时范围
+		/// </summary>
+		/// <param name="flux"></param>
+		/// <param name="start"></param>
+		/// <param name="stop"></param>
+		/// <param name="timeColumn"></param>
+		/// <returns></returns>
+		public static IFlux HourSelection(this IFlux flux, int start, int stop, string timeColumn = Column.Time)
+		{
+			return flux.Pipe($"hourSelection(start: {start}, stop: {stop}, timeColumn: '{timeColumn}')");
+		}
+	}
+}
